3b0c3aabe53e33a1b951c34e58cfa3c144fd7778
me <gocmine61@gmail.com>
2026-02-01 22:01:26 +0700

    fix: update basePath to match repo name shop-outfit12

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/my-app",
+  basePath: "/shop-outfit12",
   images: {
     unoptimized: true,
     remotePatterns: [
